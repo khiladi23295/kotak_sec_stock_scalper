e4e6328c8de7e92bfd13b329503b03771a275b43
khiladi23295 <utkarshmon@gmail.com>
2023-08-09 12:55:57 +0530

    Improved Order Management

---
+++ credentials.py
@@ -0,0 +1,8 @@
+user_details = {
+    "username" : "___",
+    "password" : "___",
+    "access_token" : "____",
+    "consumer_key" : "_____",
+    "consumer_secret" : "______",
+    "cash_used_per_stock" : 1000000
+}
--- kotak_trade_scalper.py
+++ kotak_trade_scalper.py
@@ -1,16 +1,17 @@
 from ks_api_client import ks_api
 from datetime import datetime
+import credentials as cr
 import requests
 import pandas as pd
 import math
 import time
 
-access_token = ''  # Your Access Token Here
-consumer_key = ''  # Your Consumer Key Here
-consumer_secret = ''  # Your Consumer Secret
+access_token = cr.user_details["access_token"]
+consumer_key = cr.user_details["consumer_key"]
+consumer_secret = cr.user_details["consumer_secret"]
 
-userid = ''  # Your Username Here
-password = ''  # Your Password Here
+userid = cr.user_details["username"]
+password = cr.user_details["password"]
 
 
 def exception_logs(msg, e):
@@ -32,7 +33,7 @@ login1 = client.login(password=password)
 login2 = client.session_2fa()
 
 # ----------------Getting Stocks  for the day--------------------
-df = pd.read_csv("C:/abcd/Some_Path/StockNames.txt", sep=",", header=None,
+df = pd.read_csv("C:/Users/Documents/GitHub/kotak_sec_stock_scalper/StockNames.txt", sep=",", header=None,
                  names=["First_Stock", "First_Threshold", "Second_Stock", "Second_Threshold"])
 symbol_name = [df.iloc[0, 0].upper(), df.iloc[0, 2].upper()]
 threshold_price = [df.iloc[0, 1], df.iloc[0, 3]]
@@ -76,10 +77,9 @@ for i in range(len(instrument_token_list)):
     margin_required_list.append(margin_required['Success'][i]['normal'])
 
 # ----------------Calculating quantity------------------------------
-cash_balance = 1000  # Has kept the account balance fixed for now but in future will get it through a api call
-quantity_list = [math.floor((cash_balance/margin_required_list[0])), # change to list comprehension as no. of stocks fixed
-math.floor((cash_balance/margin_required_list[1]))] # Calculating the quantity that can be bought using the cash balance in the account
-
+cash_balance = cr.user_details["cash_used_per_stock"]
+quantity_list = [math.floor(cash_balance/margin_required_list[i]) for i in range(len(margin_required_list))]
+print(quantity_list)
 # -------------------Placing the orders---------------------------
 counter = len(instrument_token_list)
 flag = 1
@@ -93,10 +93,11 @@ while flag == 1:
     if now >= my_datetime:
         msg = "Time of Execution: "
         exception_logs(msg, now)
-        for i in instrument_token_list:   # Loop that places the buy orders for both the stocks
+        for i in range(len(instrument_token_list)):# Loop that places the buy orders for both the stocks
+            inst_tok = instrument_token_list[i]
             try:
-                placed_order_details = client.place_order(order_type="MIS", instrument_token=i, transaction_type="BUY",
-                               quantity=1, price=0, disclosed_quantity=0, validity="GFD", variety="REGULAR")  # REGULAR
+                placed_order_details = client.place_order(order_type="MIS", instrument_token=inst_tok, transaction_type="BUY",
+                               quantity=quantity_list[i], price=0, disclosed_quantity=0, validity="GFD", variety="REGULAR")  # REGULAR
                 time.sleep(1)
                 msg = "Placed Order Details: "
                 exception_logs(msg, placed_order_details)
@@ -106,28 +107,32 @@ while flag == 1:
                 exception_logs(msg, e)
         
         # ----------------Placing the TP/SL orders-----------------------------
+        time.sleep(2)
         order_details = client.positions(position_type="TODAYS")
-        time.sleep(1)
+        exception_logs("Positions Details--->> ", order_details)
         for i in range(counter):    
-            exec_price = round(order_details['Success'][i]['averageStockPrice'], 2)
+            exec_price = round(order_details['Success'][i]['buyTrdAvg'], 2)
             instrumentToken = order_details['Success'][i]['instrumentToken']
-            quantity = order_details['Success'][i]['buyTradedQtyLot']
+            index_instrumentToken = instrument_token_list.index(instrumentToken)
+            quantity = quantity_list[index_instrumentToken] #order_details['Success'][i]['buyTradedQtyLot']
+            stock_name = symbol_name[index_instrumentToken]
+            msg = "Stock Nanme, Execution Price, InsToken, Index of the insToken, Quantity -> "
+            p = stock_name + " " + str(exec_price)+" "+str(instrumentToken)+" "+str(index_instrumentToken)+" "+str(quantity)
+            exception_logs(msg, p)
             
             number = round(exec_price + (exec_price * .0075), 2)  # Setting Take Profit at 0.75%
             decimal = (round((number - math.floor(number)), 2)*100)/10
             decimal2 = round(((decimal - math.floor(decimal))/10), 2)
             take_profit = round((number - decimal2), 2)
-
-            msg = "Take Profit: "
-            exception_logs(msg, take_profit)
             
             number = round(exec_price - (exec_price * .0025), 2)  # Setting Stop Loss at 0.25%
             decimal = (round((number - math.floor(number)), 2)*100)/10
             decimal2 = round(((decimal - math.floor(decimal))/10), 2)
             stop_loss = round((number - decimal2), 2)
 
-            msg = "Stop Loss: "
-            exception_logs(msg, stop_loss)
+            msg = "Take Profit and Stop Loss for "+symbol_name[index_instrumentToken] + " : "
+            p = str(take_profit)+" "+str(stop_loss)
+            exception_logs(msg, p)
                 
             try:
                 take_profit_order = client.place_order(order_type="MIS", instrument_token=instrumentToken, transaction_type="SELL",
@@ -159,27 +164,35 @@ while flag == 1:
     if now >= my_datetime:
         flag = 0  # will break because of time check when time is less
     time.sleep(1)
-    quantity = [1, 1]
-    while len(symbol_name) > 0:
-        order_report = client.order_report()
-        orders_df = pd.DataFrame()
-        for key, values in order_report["success"][0].items():
-            orders_df[key] = [order_report["success"][i][key] for i in range(len(order_report["success"]))]
+while len(symbol_name) > 0:
+    order_report = client.order_report()
+    time.sleep(1)
+    orders_df = pd.DataFrame()
+    for key, values in order_report["success"][0].items():
+        orders_df[key] = [order_report["success"][i][key] for i in range(len(order_report["success"]))]
+    remove_list = []
+    # orders_df.to_csv("C:/Users/Documents/GitHub/kotak_sec_stock_scalper/orders_df.csv", mode='a')
+    for i in range(len(symbol_name)):
+        temp_df = orders_df[orders_df["instrumentName"] == symbol_name[i]]
+        # temp_df.to_csv("C:/Users/Documents/GitHub/kotak_sec_stock_scalper/temp_df.csv", mode='a')
+        quant = quantity_list[i]
+        print(symbol_name[i],"Selected Quantity from list->", quant,temp_df["filledQuantity"])
+        if list(temp_df["filledQuantity"]) in [[quant, quant, 0], [quant, 0, quant],[0, quant, quant]]:
+            a = int(temp_df["orderId"].where(temp_df["filledQuantity"] == 0).dropna().iloc[0])
+            print(a)
+            client.cancel_order(order_id=a)
+            orders_df.drop(orders_df[orders_df["instrumentName"] == symbol_name[i]].index, inplace=True)
+            remove_list.append(symbol_name[i])
+            msg = "Extra Order Cancelled for "+ symbol_name[i]+" with orderid "
+            exception_logs(msg, a)
 
-        remove_list = []
-        for i in range(len(symbol_name)):
-            temp_df = orders_df[orders_df["instrumentName"] == symbol_name[i]]
-            quant = quantity[i]
-            print(symbol_name[i])
-            if (list(temp_df["filledQuantity"]) in [[quant, quant, 0], [quant, 0, quant]]):
-                a = int(temp_df["orderId"].where(temp_df["filledQuantity"] == 0).dropna().iloc[0])
-                print(a)
-                client.cancel_order(order_id=a)
-                orders_df.drop(orders_df[orders_df["instrumentName"] == symbol_name[i]].index, inplace=True)
-                remove_list.append(symbol_name[i])
-            else:
-                print("TP/SL is not filled")
-        if len(remove_list) > 0:
-            for i in remove_list:
-                symbol_name.remove(i)
-client.logout()  # Logout the session created through  Kotak API
+        else:
+            print("TP/SL is not filled")
+    if len(remove_list) > 0:
+        for i in remove_list:
+            index = symbol_name.index(i)
+            symbol_name.remove(i)
+            quantity_list.pop(index)
+    print("Remove_list --> ", remove_list)
+    time.sleep(1)
+print(client.logout())  # Logout the session created through  Kotak API
